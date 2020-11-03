df69368888cc9e3351166af6f1f8a24eb58dca4d
bogay <pojay11523@gmail.com>
2020-11-03 23:09:39 +0800

    Finish player animation script

--- Assets/Scripts/Entity/Player.cs
+++ Assets/Scripts/Entity/Player.cs
@@ -7,7 +7,7 @@ using UnityEngine.Serialization;
 
 namespace Entity
 {
-    public class Player : EntityEventListener<IPlayer> , IDestroyable
+    public class Player : EntityEventListener<IPlayer>, IDestroyable
     {
         private bool up;
         private bool down;
@@ -16,13 +16,13 @@ namespace Entity
         private PlayerAppearance playerAppearance;
         public int playerId;
         private int health = 100;
-        
+
         #region Shoot_Manipulation
-        
+
         [SerializeField] private float shootDelay = 0.5f;
         private bool canShoot = true;
         private bool triggerShoot = false;
-        
+
         private IEnumerator ShootIdle()
         {
             canShoot = false;
@@ -33,16 +33,14 @@ namespace Entity
         private Vector2 DetectShoot()
         {
             var mousePos = Input.mousePosition;
-            if (Camera.main == null) throw new ArgumentException("can not found main camera");
-            
+            if(Camera.main == null) throw new ArgumentException("can not found main camera");
             mousePos.z = Camera.main.nearClipPlane;
             StartCoroutine(ShootIdle());
             return (Camera.main.ScreenToWorldPoint(mousePos) - transform.position).normalized;
         }
 
         #endregion
-        
-        
+
         private void Awake()
         {
             playerAppearance = GetComponent<PlayerAppearance>();
@@ -50,17 +48,19 @@ namespace Entity
 
         public override void Attached()
         {
-            state.SetTransforms(state.Transform,transform);
+            state.SetTransforms(state.Transform, transform);
+            state.SetAnimator(GetComponent<Animator>());
         }
+
         private void PollKeys()
         {
-            up= Input.GetKey(KeyCode.W);
+            up = Input.GetKey(KeyCode.W);
             down = Input.GetKey(KeyCode.S);
             left = Input.GetKey(KeyCode.A);
             right = Input.GetKey(KeyCode.D);
             triggerShoot = Input.GetMouseButton(0);
         }
-        
+
         private void Update()
         {
             PollKeys();
@@ -68,18 +68,18 @@ namespace Entity
 
         public override void SimulateController()
         {
-            if (!(up || down || left || right || (canShoot && triggerShoot)))
+            if(!(up || down || left || right || (canShoot && triggerShoot)))
                 return;
-                
+
             var input = PlayerCmd.Create();
-            
+
             input.Up = up;
             input.Down = down;
             input.Left = left;
             input.Right = right;
             input.Attack = false;
-            
-            if (canShoot && triggerShoot)
+
+            if(canShoot && triggerShoot)
             {
                 input.AtkDir = DetectShoot();
                 input.Attack = true;
@@ -94,20 +94,20 @@ namespace Entity
                 BoltLog.Error(e);
                 entity.ClearInputQueue();
             }
-            
+
         }
 
         public override void MissingCommand(Command previous)
         {
-            if (previous == null) return;
-            ExecuteCommand(previous,true);
+            if(previous == null) return;
+            ExecuteCommand(previous, true);
         }
 
         public override void ExecuteCommand(Command command, bool resetState)
         {
             var cmd = (PlayerCmd) command;
 
-            if (resetState)
+            if(resetState)
             {
                 // we got a correction from the server, reset (this only runs on the client)
                 playerAppearance.transform.position = cmd.Result.Axes;
@@ -115,19 +115,19 @@ namespace Entity
             else
             {
                 var mvDir = Vector3.zero;
-                if(cmd.Input.Down) mvDir+= Vector3.down;
-                if(cmd.Input.Up) mvDir+= Vector3.up;
-                if(cmd.Input.Left) mvDir+= Vector3.left;
-                if(cmd.Input.Right) mvDir+= Vector3.right;
-                
+                if(cmd.Input.Down) mvDir += Vector3.down;
+                if(cmd.Input.Up) mvDir += Vector3.up;
+                if(cmd.Input.Left) mvDir += Vector3.left;
+                if(cmd.Input.Right) mvDir += Vector3.right;
+
                 // apply movement (this runs on both server and client)
                 playerAppearance.Move(mvDir.normalized);
-                
+
                 // copy the motor state to the commands result (this gets sent back to the client)
                 cmd.Result.Axes = playerAppearance.transform.position;
-                
+
                 if(BoltNetwork.IsClient) return;
-                if (!cmd.Input.Attack || !canShoot) return;
+                if(!cmd.Input.Attack || !canShoot) return;
                 StartCoroutine(ShootIdle());
                 Bullet.SpawnBullet(playerId, cmd.Input.AtkDir, transform.position);
             }
@@ -138,21 +138,22 @@ namespace Entity
         {
             BoltLog.Warn($"player {playerId} get hurt for {dmg}");
             health -= dmg;
-            
-            var dmgEvent = OnDamaged.Create(entity ,EntityTargets.OnlyController);
-            dmgEvent.TargetID = Convert.ToInt32(playerId) ;
+
+            var dmgEvent = OnDamaged.Create(entity, EntityTargets.OnlyController);
+            dmgEvent.TargetID = Convert.ToInt32(playerId);
             dmgEvent.Dmg = dmg;
             dmgEvent.Send();
 
-            if (health > 0) return;
+            if(health > 0) return;
             OnPlayerDeath.Post(GlobalTargets.AllClients, playerId);
             PlayerRegistry.RemovePlayer(Convert.ToUInt32(playerId));
         }
+
         public override void OnEvent(OnDamaged evnt)
         {
             BoltLog.Warn($"target id {evnt.TargetID} get hurt for {evnt.Dmg}");
         }
-        
+
         public void OnUpdateBag(string bagContent)
         {
             BoltLog.Warn("sending update bag event");
--- Assets/Scripts/Entity/PlayerAppearance.cs
+++ Assets/Scripts/Entity/PlayerAppearance.cs
@@ -5,9 +5,30 @@ namespace Entity
     public class PlayerAppearance : MonoBehaviour
     {
         [SerializeField] private float moveScale = 0.5f;
+        private Animator animator;
+        private float moveSpeed = 0;
+
+        private void Start()
+        {
+            this.animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            this.moveSpeed -= 10 * Time.deltaTime;
+            this.moveSpeed = Mathf.Max(0, this.moveSpeed);
+            animator.SetFloat("xSpeed", this.moveSpeed);
+        }
+
         public void Move(Vector3 mvDir)
         {
-            transform.position += mvDir * moveScale;
+            transform.position += mvDir * moveScale * Time.deltaTime;
+            // setup rotation
+            var rot = transform.eulerAngles;
+            rot.y = mvDir.x > 0 ? 0 : 180;
+            transform.eulerAngles = rot;
+            // set speed
+            this.moveSpeed = 1;
         }
     }
 }
